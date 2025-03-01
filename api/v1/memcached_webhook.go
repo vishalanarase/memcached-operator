2247fc9aca04f9aae713871dc8844844a3ac072f
Vishal Anarase <iamvishalanarase@gmail.com>
2025-03-01 16:43:21 +0530

    Implement mutating and validating webhooks
    Signed-off-by: Vishal Anarase <iamvishalanarase@gmail.com>

--- api/v1/memcached_webhook.go
+++ api/v1/memcached_webhook.go
@@ -17,6 +17,8 @@ limitations under the License.
 package v1
 
 import (
+	"errors"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -34,8 +36,6 @@ func (r *Memcached) SetupWebhookWithManager(mgr ctrl.Manager) error {
 		Complete()
 }
 
-// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-
 // +kubebuilder:webhook:path=/mutate-cache-devspace-com-v1-memcached,mutating=true,failurePolicy=fail,sideEffects=None,groups=cache.devspace.com,resources=memcacheds,verbs=create;update,versions=v1,name=mmemcached.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Defaulter = &Memcached{}
@@ -44,12 +44,11 @@ var _ webhook.Defaulter = &Memcached{}
 func (r *Memcached) Default() {
 	memcachedlog.Info("default", "name", r.Name)
 
-	// TODO(user): fill in your defaulting logic.
+	if r.Spec.Size == 0 {
+		r.Spec.Size = 3
+	}
 }
 
-// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
-// NOTE: The 'path' attribute must follow a specific pattern and should not be modified directly here.
-// Modifying the path for an invalid path can cause API server errors; failing to locate the webhook.
 // +kubebuilder:webhook:path=/validate-cache-devspace-com-v1-memcached,mutating=false,failurePolicy=fail,sideEffects=None,groups=cache.devspace.com,resources=memcacheds,verbs=create;update,versions=v1,name=vmemcached.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Validator = &Memcached{}
@@ -58,22 +57,26 @@ var _ webhook.Validator = &Memcached{}
 func (r *Memcached) ValidateCreate() (admission.Warnings, error) {
 	memcachedlog.Info("validate create", "name", r.Name)
 
-	// TODO(user): fill in your validation logic upon object creation.
-	return nil, nil
+	return nil, validateOdd(r.Spec.Size)
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *Memcached) ValidateUpdate(old runtime.Object) (admission.Warnings, error) {
 	memcachedlog.Info("validate update", "name", r.Name)
 
-	// TODO(user): fill in your validation logic upon object update.
-	return nil, nil
+	return nil, validateOdd(r.Spec.Size)
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *Memcached) ValidateDelete() (admission.Warnings, error) {
 	memcachedlog.Info("validate delete", "name", r.Name)
 
-	// TODO(user): fill in your validation logic upon object deletion.
 	return nil, nil
+}
+
+func validateOdd(n int) error {
+	if n%2 == 0 {
+		return errors.New("cluster size must be an odd number")
+	}
+	return nil
 }
