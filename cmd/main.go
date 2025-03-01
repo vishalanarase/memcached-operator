a0e0183f55d0cb1ced1ac811dd1b10d12ddc3db0
Vishal Anarase <iamvishalanarase@gmail.com>
2025-03-01 16:11:47 +0530

    Add event recorder
    Signed-off-by: Vishal Anarase <iamvishalanarase@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -145,8 +145,9 @@ func main() {
 	}
 
 	if err = (&controller.MemcachedReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
+		Client:   mgr.GetClient(),
+		Scheme:   mgr.GetScheme(),
+		Recorder: mgr.GetEventRecorderFor("memcached-controller"),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "Memcached")
 		os.Exit(1)
